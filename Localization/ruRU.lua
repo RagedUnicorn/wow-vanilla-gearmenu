ed4211fbfc6521449ab802017de4641a9243102b
Артур <Artur91425@gmail.com>
2018-02-08 01:25:38 +0300

    Added ruRU localization

---
+++ Localization/ruRU.lua
@@ -0,0 +1,66 @@
+-- Russian localization by Lichery
+
+gm.L["name"] = "GearMenu"
+
+-- console
+gm.L["help"] = "|cFFFFFF00(%s)|r: Используйте |cFFFFFF00/gm|r или |cFFFFFF00/gearmenu|r для списка опций"
+gm.L["show"] = "|cFFFFFF00show|r - показать "
+gm.L["hide"] = "|cFFFFFF00hide|r - скрыть "
+gm.L["opt"] = "|cFFFFFF00opt|r - показать Optionsmenu"
+gm.L["reload"] = "|cFFFFFF00reload|r - перезагрузить UI"
+
+-- user errors
+gm.L["unequip_failed"] = "Нет свободного места в сумках"
+gm.L["quick_change_item_select_missing"] = "You have to select an item in both from and to list"
+
+-- gui
+gm.L["navigationslots"] = "Ячейки"
+gm.L["navigationgeneral"] = "Общее"
+gm.L["navigationquickchange"] = "Быстрая смена"
+gm.L["navigationabout"] = "Об аддоне"
+
+-- slots tab
+gm.L["titleslot1"] = "Ячейка 1:"
+gm.L["titleslot2"] = "Ячейка 2:"
+gm.L["titleslot3"] = "Ячейка 3:"
+gm.L["titleslot4"] = "Ячейка 4:"
+gm.L["titleslot5"] = "Ячейка 5:"
+gm.L["titleslot6"] = "Ячейка 6:"
+gm.L["titleslot7"] = "Ячейка 7:"
+gm.L["slot_menu_slot_already_in_use"] = "Ячейка уже используется"
+
+-- general tab
+gm.L["lockwindow"] = "Блокировка окна"
+gm.L["lockwindowtooltip"] = "Запрещает перемещение окна GearMenu."
+gm.L["showcooldowns"] = "Показать восстановление"
+gm.L["showcooldownstooltip"] = "Показывает восстановление для всех предметов."
+gm.L["showkeybindings"] = "Показять горячие клавиши"
+gm.L["showkeybindingstooltip"] = "Показывает привязки клавиш по одетым предметам."
+gm.L["showtooltips"] = "Отключить подсказки"
+gm.L["showtooltipstooltip"] = "Отключает всплывающую подсказку."
+gm.L["smalltooltips"] = "Показать маленькие подсказки"
+gm.L["smalltooltipstooltip"] = "Показывает только название предмета, а не полную всплывающую подсказку."
+gm.L["disabledraganddrop"] = "Отключить перетаскивание"
+gm.L["disabledraganddroptooltip"] = "Отключает перетаскивание для предметов."
+gm.L["filteritemquality"] = "Фильтр качества предметов:"
+gm.L["item_quality_poor"] = "Низкий (Серый)"
+gm.L["item_quality_common"] = "Обычный (Белый)"
+gm.L["item_quality_uncommon"] = "Необычный (Зеленый)"
+gm.L["item_quality_rare"] = "Редкий (Синий)"
+gm.L["item_quality_epic"] = "Превосходный (Фиолетовый)"
+gm.L["item_quality_legendary"] = "Легендарный (Оранжевый)"
+
+-- quick change tab
+gm.L["rulelistlabel"] = "Правила:"
+gm.L["changefromlabel"] = "Из:"
+gm.L["changetolabel"] = "В:"
+gm.L["delaylabel"] = "Задержка: "
+gm.L["delayunitlabel"] = "сек"
+gm.L["addrule"] = "Добавить правило"
+gm.L["deleterule"] = "Удалить правило"
+
+-- about tab
+gm.L["author"] = "Автор: Michael Wiesendanger"
+gm.L["email"] = "E-Mail: michael.wiesendanger@gmail.com"
+gm.L["version"] = "Версия: " .. GM_CONSTANTS.ADDON_VERSION
+gm.L["issues"] = "Баг трекер: https://github.com/RagedUnicorn/wow-gearmenu/issues"
