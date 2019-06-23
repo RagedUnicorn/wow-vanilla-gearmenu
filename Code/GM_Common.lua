d6dd00a4e38c233551a22da47d1884dfcff8f706
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2019-06-23 18:42:07 +0200

    Eliminate global position variable in common module

--- Code/GM_Common.lua
+++ Code/GM_Common.lua
@@ -54,7 +54,7 @@ end
   @return {number}
 ]]--
 function me.ExtractPositionFromName(name)
-  _, _, position = strfind(name, "(%d+)")
+  local _, _, position = strfind(name, "(%d+)")
 
   return tonumber(position)
 end
