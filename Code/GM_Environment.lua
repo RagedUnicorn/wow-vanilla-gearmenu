109491333163d1726e63094c83d631951ea226ef
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2023-08-01 22:59:28 +0200

    Update addon version to 1.2.2

--- Code/GM_Environment.lua
+++ Code/GM_Environment.lua
@@ -30,7 +30,7 @@
 GM_ENVIRONMENT = {
   ADDON_NAME = "GearMenu",
   ADDON_NAME_SHORT = "GM",
-  ADDON_VERSION = "1.2.1",
+  ADDON_VERSION = "1.2.2",
   ADDON_IDENTIFIER = "com.ragedunicorn.wow.vanilla.gearmenu",
   LOG_LEVEL = 4,
   LOG_EVENT = true
